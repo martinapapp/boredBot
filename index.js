46f67993db64b3cc559c7c953a3e3deff13c055e
Martina Papp <martinapapp0@gmail.com>
2026-03-10 13:13:40 +0100

    refactor: add json placeholder

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 document.getElementById("get-activity").addEventListener("click", function() {
-  fetch("https://apis.scrimba.com/bored/api/activity")
+  const randomId = Math.floor(Math.random() * 200) + 1
+
+  fetch(`https://jsonplaceholder.typicode.com/todos/${randomId}`)
     .then(response => response.json())
     .then(data => {
-      document.getElementById("activity").textContent = data.activity
+      document.getElementById("activity").textContent = data.title
       document.getElementById("title").textContent = "HappyBot"
       document.querySelector("main").classList.add("fun")
     })
