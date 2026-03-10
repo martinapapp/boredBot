b5208a6f2090990b6ea26724a3038c47766bd2e7
Martina Papp <martinapapp0@gmail.com>
2026-03-10 13:49:32 +0100

    fix: add jsonplaceholder for fetch

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function getActivityIdea() {
-    fetch("https://apis.scrimba.com/bored/api/activity")
-        .then(res => res.json())
-        .then(data => {
-            document.getElementById("idea").textContent = data.activity
-            document.body.classList.add("fun")
-            document.getElementById("title").textContent = "HappyBot"
-        })
+  const randomId = Math.floor(Math.random() * 200) + 1
+
+  fetch(`https://jsonplaceholder.typicode.com/todos/${randomId}`)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("idea").textContent = data.title
+      document.body.classList.add("fun")
+      document.getElementById("title").textContent = "HappyBot"
+    })
 }
 
-document.getElementById("bored-button").addEventListener("click", getActivityIdea)
+document.getElementById("get-activity").addEventListener("click", getActivityIdea)
